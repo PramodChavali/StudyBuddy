3f2d9cd3b6e92d617b06aad620b0a767b6e24e8f
Pramod Chavali <pramod.chavali17@gmail.com>
2025-05-28 21:26:59 -0400

    doing all of it
    it works now, just need to build and playtest

--- questions.py
+++ questions.py
@@ -1,15 +1,75 @@
 from tkinter import *
+from time import time, sleep
+from google import genai as gemini
+from random import uniform
+import os
+from dotenv import load_dotenv
 
-root = Tk()
-root.title("START STUDYING")
-screen = Canvas(root, width=1920, height=1080, bg="#2a243b")
-screen.pack()
-
-
-
-while True:
-    root.attributes("-fullscreen", True)
+load_dotenv()
+apiKey = os.getenv("API_KEY")
 
 
 
-    screen.mainloop()
+####################################################################################
+
+
+file = open("frequency.txt", "r")
+numMins = int(file.read(-1))
+file.close()
+
+startTime = time()
+
+#subject and grade
+file = open("subjects.txt", "r")
+subject = file.read(-1)
+file.close()
+
+file = open("grade.txt", "r")
+grade = int(file.read(-1))
+file.close()
+
+####################################################################################
+
+
+
+
+def SetUpQuestion(prompt):
+    client = gemini.Client(api_key=apiKey)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents= (prompt),
+
+    )
+
+    return response.text
+
+def main(prompt):
+    geminiResponse = SetUpQuestion(prompt)
+    print(geminiResponse)
+
+    question = geminiResponse.split("|")[0].strip()
+    answerA = geminiResponse.split("|")[1].strip()
+    answerB = geminiResponse.split("|")[2].strip()
+    answerC = geminiResponse.split("|")[3].strip()
+
+    print(question)
+    print(answerA)
+    print(answerB)
+    print(answerC)
+
+    file = open("questions/question.txt", "w")
+    file.write(question)   
+    file.close()
+
+    file = open("questions/answerA.txt", "w")
+    file.write(answerA)
+    file.close()
+
+    file = open("questions/answerB.txt", "w")
+    file.write(answerB)
+    file.close()
+
+    file = open("questions/answerC.txt", "w")
+    file.write(answerC)
+    file.close()
+
---
+++ setup.py
@@ -0,0 +1,113 @@
+from tkinter import *
+from subprocess import Popen
+
+root = Tk()
+root.title("START STUDYING SETUP")
+screen = Canvas(root, width=800, height=600, bg="#2a243b")
+screen.pack()
+
+questionStage = 0
+selectedOption = StringVar()
+selectedOption.set("")
+
+def showGradeQuestion():
+    #get the grade of the user
+    screen.delete("all")
+    showQuestions("what grade are you in?", "", "")
+
+    #dropwdown menu
+    dropdownOptions = ["9", "10", "11", "12"]
+    selectedOption.set(dropdownOptions[0])
+    dropdown = OptionMenu(root, selectedOption, *dropdownOptions)
+    screen.create_window(400, 400, window=dropdown)
+
+    #submit button
+    submitButton = Button(root, text="Submit", font="arial 20", command=onGradeSubmit)
+    screen.create_window(400, 500, window=submitButton)
+
+def showFrequencyQuestion():
+    #get how often we can troll the user
+    screen.delete("all")
+    showQuestions("How often would you", "like to get questions?", " (in minutes)")
+
+    #dropdown menu
+    dropdownOptions = ["5", "10", "15", "20"]
+    selectedOption.set(dropdownOptions[0])
+    dropdown = OptionMenu(root, selectedOption, *dropdownOptions)
+    screen.create_window(400, 400, window=dropdown)
+
+
+    #submit button
+    submitButton = Button(root, text="Submit", font="arial 20", command=onFrequencySubmit)
+    screen.create_window(400, 500, window=submitButton)
+
+def showSubjectQuestion():
+
+    #depnding on the grade, give different subjects
+    #get the grade
+
+    file = open("grade.txt", "r")
+    grade = int(file.read(-1))
+    file.close()
+
+    if grade == 9:
+        dropdownOptions = ["Math", "Science", "Geography", "French"]
+
+    elif grade == 10:
+        dropdownOptions = ["Math", "Science", "Buisiness", "Careers", "Civics", "French"]
+
+    elif grade == 11:
+        dropdownOptions = ["Functions", "Functions and Applications", "Foundations for College Math", "Intro to Anth, Soch, and Psych", "Physics", "Chemistry", "Biology", "Earth and Space Science", "French", "Natural Disasters", "Crimes against Humanity", "World History to the 15th Century", "Law","Marketing", "Accounting", "Entrepreneurship"]
+
+    elif grade == 12:
+        dropdownOptions = ["Calc and Vectors", "Foundations for College Math", "Advanced Functions", "Data Management", "Earth and Space Science", "Medical Sciences", "Physics", "Chemistry", "Biology", "French", "International Buisness", "Accounting", "Buisiness Leadership", "Politics", "Law", "Canadian and World Issues", "History since the 15th century"]
+
+
+    #what subjects they want to study
+    screen.delete("all")
+    showQuestions("What subjects would you", "like to study?", "")
+    
+    #dropdown menu
+    
+    selectedOption.set(dropdownOptions[0])
+    dropdown = OptionMenu(root, selectedOption, *dropdownOptions)
+    screen.create_window(400, 400, window=dropdown)
+
+    #submit button
+    submitButton = Button(root, text="Submit", font="arial 20", command=onSubjectSubmit)
+    screen.create_window(400, 500, window=submitButton)
+
+def endSetup():
+    screen.delete("all")
+    showQuestions("Setup Complete!", "Window will close", "in 2 seconds")
+    root.after(2000, root.destroy)  # Close the window after 2 seconds
+    Popen(["python", "main.py"])
+
+def showQuestions(question, question2, question3):
+    screen.create_text(400, 100, text=question, fill="white", font=("Arial", 50), anchor="center")
+    screen.create_text(400, 200, text=question2, fill="white", font=("Arial", 50), anchor="center")
+    screen.create_text(400, 300, text=question3, fill="white", font=("Arial", 50), anchor="center")
+
+def saveToFile(filepath, answer):
+    with open(filepath, "w") as file:
+        file.write(str(answer))
+
+def onGradeSubmit():
+    print("selected grade: ", selectedOption.get())
+    saveToFile("grade.txt", selectedOption.get())
+    showFrequencyQuestion()
+
+def onFrequencySubmit():
+    print("selected frequency: ", selectedOption.get())
+    saveToFile("frequency.txt", selectedOption.get())
+    showSubjectQuestion()
+
+
+def onSubjectSubmit():
+    print("selected subjects: ", selectedOption.get())
+    saveToFile("subjects.txt", selectedOption.get())
+    endSetup()
+
+
+showGradeQuestion()
+root.mainloop()
